944b1715df669524e100ef8fd4d98c9ae19d4f94
Mounika Neerati <mounika.neerati93@gmail.com>
2023-10-04 11:53:23 +0530

    neste components

---
+++ App.js
@@ -0,0 +1,15 @@
+
+function MyButton(){
+    return(
+        <button>I'am a Button</button>
+    )
+}
+
+export default function App(){
+    return(
+        <div>
+        <h1>Welcome To My App</h1>
+        <MyButton/>
+        </div>
+    )
+}
